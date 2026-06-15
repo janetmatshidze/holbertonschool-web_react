a58e535c71e8481020afc1971c153cd29897b91f
janetmatshidze <matshidzajanet@gmail.com>
2026-06-15 13:57:05 +0200

    0. Basic application

--- react_intro/task_0/dashboard/src/App.jsx
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -2,9 +2,8 @@ import holbertonLogo from './assets/holberton-logo.jpg'
 import './App.css'
 
 function App() {
-
   return (
-    <>
+    <div className="App">
       <div className="App-header">
       <img className= "App-logo" src={holbertonLogo} alt="holberton logo"/>
         <h1>School dashboard</h1>
@@ -13,9 +12,9 @@ function App() {
         <p>Login to access the full dashboard</p>
       </div>
       <div className="App-footer">
-        <p>Copyright 2024 - holberton School</p>
+        <p>Copyright 2026 - Holberton School</p>
       </div>
-    </>
+      </div>
   )
 }
 
